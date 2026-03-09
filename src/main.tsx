119ebe0c33b32f13f82d1e5b649e4732ab031f43
CooperWilks0501 <cwilks0501@outlook.com>
2026-03-09 16:20:15 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MHCUpdatedWebApllication">
       <App />
     </BrowserRouter>
   </React.StrictMode>
